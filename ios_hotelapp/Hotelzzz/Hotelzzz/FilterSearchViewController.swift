b0a0bc0b4bf17ea5c49d32a12366a4b4f065c7cc
Colin Ferris <colin@colinferris.com>
2017-06-13 21:34:27 -0400

    Added minimum price slider to filter screen.

--- ios_hotelapp/Hotelzzz/Hotelzzz/FilterSearchViewController.swift
+++ ios_hotelapp/Hotelzzz/Hotelzzz/FilterSearchViewController.swift
@@ -12,8 +12,10 @@ typealias PriceRange = (min: Double, max: Double)
 
 class FilterSearchViewController: UIViewController {
     @IBOutlet weak var minimumPriceLabel: UILabel!
+    @IBOutlet weak var minimumPriceSlider: UISlider!
+    
     @IBOutlet weak var maximumPriceLabel: UILabel!
-    @IBOutlet weak var priceSlider: UISlider!
+    @IBOutlet weak var maximumPriceSlider: UISlider!
     
     var priceRange: PriceRange!
     
@@ -23,22 +25,52 @@ class FilterSearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.minimumPriceLabel.text = priceRange.min.asCurrency()
-        self.maximumPriceLabel.text = priceRange.max.asCurrency()
-        
         let minValue = Float(priceRange.min)
         let maxValue = Float(priceRange.max)
-        self.priceSlider.minimumValue = minValue
-        self.priceSlider.maximumValue = maxValue
+        
+        // configure minimum price label and slider
+        minimumPriceLabel.text = priceRange.min.asCurrency()
+        minimumPriceSlider.minimumValue = minValue
+        minimumPriceSlider.maximumValue = maxValue
+        
+        minimumPriceSlider.setValue(minValue, animated: false)
+        
+        // configure maximum price label and slider
+        maximumPriceLabel.text = priceRange.max.asCurrency()
+        maximumPriceSlider.minimumValue = minValue
+        maximumPriceSlider.maximumValue = maxValue
+        
+        maximumPriceSlider.setValue(maxValue, animated: false)
+    }
+    
+    @IBAction func minValueChanged(_ sender: UISlider) {
+        // Protects min slider value from being greater than max slider.
+        // This prevents an invalid range.
+        guard minimumPriceSlider.value < maximumPriceSlider.value else {
+            minimumPriceSlider.setValue(maximumPriceSlider.value, animated: false)
+            return
+        }
+        
+        let selectedValue = Double(sender.value)
+        minimumPriceLabel.text = selectedValue.asCurrency()
     }
     
     @IBAction func maxValueChanged(_ sender: UISlider) {
+        // Protects max slider value from being less than min slider.
+        // This prevents an invalid range.
+        guard maximumPriceSlider.value > minimumPriceSlider.value else {
+            maximumPriceSlider.setValue(minimumPriceSlider.value, animated: false)
+            return
+        }
+        
         let selectedValue = Double(sender.value)
         maximumPriceLabel.text = selectedValue.asCurrency()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        priceRange.max = Double(priceSlider.value)
+        priceRange.min = Double(minimumPriceSlider.value)
+        priceRange.max = Double(maximumPriceSlider.value)
+        
         onCompletion(priceRange)
     }
 }
